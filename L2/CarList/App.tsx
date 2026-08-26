b671c424314e71cb11a9faa7aca82c3b4e32815a
Asta Knudsen <asta.strachottak@gmail.com>
2026-08-26 17:27:12 +0200

    todo for carlist

--- L2/CarList/App.tsx
+++ L2/CarList/App.tsx
@@ -1,6 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
+TODO:
+Create a file named CarItem.tsx.
+Define an interface for the props the CarItem component will receive.
+Create a minimal view that displays the information from the props.
+
+In App.tsx:
+Create a list of data of cars, that will be displayed by the list, e.g.
+  const cars = [{id: 1, type:"Mercedes", mileage:1000, price: 5000}, {...},{...}];
+Use a list of your choice, eg. FlatList, and provide it with data (list defined)
+and a renderItems function. Render the list as CarItem components.
+ */
+
 export default function App() {
   return (
     <View style={styles.container}>
